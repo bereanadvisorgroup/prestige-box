2ff13578ef3a7222c74adc159f27ac070d131e0f
Matt Duvall <mduvall@gmail.com>
2026-07-06 15:24:19 -0400

    Revert "added web protections"
    This reverts commit 7b88f7b42e221061cfd973f31a8ee6e5cd91f92b.

--- next.config.mjs
+++ next.config.mjs
@@ -16,19 +16,6 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Content-Security-Policy", value: "frame-ancestors 'none'" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        ],
-      },
-    ];
-  },
 };
 
 export default withAxiom(nextConfig);
